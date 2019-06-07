93e642ebfdc6d50033c395d43d210bca2ef73fad
Max Reeves <maxreeves@Maxs-MacBook-Pro.local>
2019-06-07 12:59:59 +1000

    added users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/index'
+  get 'users/new'
+  get 'users/show'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'recipes/edit'
