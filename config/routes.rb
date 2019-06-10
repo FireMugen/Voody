3c3ac26c0ef113ee17700ec56b5a406f33bc9828
FireMugen <maxxreeves@icloud.com>
2019-06-10 19:29:43 +1000

    bootstrapped

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,13 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/new'
   get 'users/show'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/home'
-  get 'static_pages/help'
   get 'recipes/edit'
   get 'recipes/new'
   get 'recipes/show'
 
+  get  '/signup',  to: 'users#new'
+	get  '/help',    to: 'static_pages#help'
+	get  '/about',   to: 'static_pages#about'
+	get  '/contact', to: 'static_pages#contact'
   root :to => 'static_pages#home'
 end
