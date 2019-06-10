cece3410e90b30303bf577cbcef898b650a3d6a1
FireMugen <maxxreeves@icloud.com>
2019-06-10 20:19:52 +1000

    working sign up

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,12 @@ Rails.application.routes.draw do
   get 'recipes/show'
 
   get  '/signup',  to: 'users#new'
+	post '/signup',  to: 'users#create'
+	
 	get  '/help',    to: 'static_pages#help'
 	get  '/about',   to: 'static_pages#about'
 	get  '/contact', to: 'static_pages#contact'
   root :to => 'static_pages#home'
+	resources :users
+
 end
