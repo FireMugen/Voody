d192ac836e16a9c794e434be848aa037484a4298
FireMugen <maxxreeves@icloud.com>
2019-06-11 11:58:29 +1000

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,28 @@
 class UsersController < ApplicationController
-	attr_accessor :name, :email
 
-  def edit
-  end
-
-  def index
+  def show
+    @user = User.find(params[:id])
   end
 
   def new
-		@user = User.new
+    @user = User.new
   end
 
-	def show
-	@user = User.find(params[:id])
-	end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
 
-	def create
-	  @user = User.new(user_params)
-		if @user.save
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-		else
-			render 'new'
-		end
-	end
+  private
 
-	private
-
-	def user_params
-		params.require(:user).permit(:name, :email, :password,
-																 :password_confirmation)
-	end
-
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/edit'
   get 'users/index'
-  get 'users/new'
+
   get 'users/show'
   get 'recipes/edit'
   get 'recipes/new'
@@ -9,11 +10,16 @@ Rails.application.routes.draw do
 
   get  '/signup',  to: 'users#new'
 	post '/signup',  to: 'users#create'
-	
+
+	get    '/login',   to: 'sessions#new'
+	post   '/login',   to: 'sessions#create'
+	delete '/logout',  to: 'sessions#destroy'
+
 	get  '/help',    to: 'static_pages#help'
 	get  '/about',   to: 'static_pages#about'
 	get  '/contact', to: 'static_pages#contact'
-  root :to => 'static_pages#home'
+
+	root :to => 'static_pages#home'
 	resources :users
 
 end
